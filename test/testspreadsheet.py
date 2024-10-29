fd6b44c8c735a4d01b50fe12d9617b5f55af6327
lottarantala <lottarantala20@gmail.com>
2024-10-29 18:07:16 +0200

    US4 - test 2

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -44,3 +44,9 @@ class TestSpreadSheet(TestCase):
         spreadsheet.set("A1", "=B1")
         spreadsheet.set("B1", "42")
         self.assertEqual(42, spreadsheet.evaluate("A1"))
+
+    def test_reference_with_non_valid_integer(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "=B1")
+        spreadsheet.set("B1", "42.5")
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
