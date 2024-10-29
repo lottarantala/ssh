74ee9de65d9c80477e7ddafd1dba754871b05614
lottarantala <lottarantala20@gmail.com>
2024-10-29 17:22:44 +0200

    US1 - test 1

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,3 +1,4 @@
+
 class SpreadSheet:
 
     def __init__(self):
@@ -10,5 +11,16 @@ class SpreadSheet:
     def get(self, cell: str) -> str:
         return self._cells.get(cell, '')
 
-    def evaluate(self, cell: str) -> int | str:
-        pass
+    def evaluate(self, cell: str):
+        if cell in self._evaluating:
+            raise ValueError("Circular dependency detected")
+        self._evaluating.add(cell)
+        try:
+            value = self._cells.get(cell, '')
+            if value.isdigit():
+                return int(value)
+            else:
+                return value
+        finally:
+            self._evaluating.remove(cell)
+
